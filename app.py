3b02130bcaf03aa8b875b3d6a9b99185e34bc212
Abhinandan Roul <53302086+AbhinandanRoul@users.noreply.github.com>
2021-03-07 15:32:07 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,29 @@
-from chatterbot import ChatBot
 from flask import Flask, render_template, request
+import os
+from chatterbot import ChatBot
+from chatterbot.trainers import ListTrainer
 from chatterbot.trainers import ChatterBotCorpusTrainer
 
-# chatbot=ChatBot("Ellie",storage_adapter="chatterbot.storage.MongoDatabaseAdapter")
-chatbot = ChatBot('Ellie')
-trainer = ChatterBotCorpusTrainer(chatbot)
-trainer.train("chatterbot.corpus.english")
+chatbot = ChatBot(
+    'Ellie',
+    storage_adapter='chatterbot.storage.SQLStorageAdapter',
+    logic_adapters=[
+        'chatterbot.logic.BestMatch',
+        {
+            'import_path': 'chatterbot.logic.BestMatch',
+            'default_response': 'Sorry, I don\'t get it.',
+            'maximum_similarity_threshold': 0.90
+        }
+    ],
+    database_uri='sqlite:///database.sqlite3'
+)
+# Training with Personal Ques & Ans
+training_data_questions = open('static/health.txt').read().splitlines()
+training_data = training_data_questions
+trainer = ListTrainer(chatbot)
+trainer.train(training_data)
+# Training with English Corpus Data
+trainer_corpus = ChatterBotCorpusTrainer(chatbot)
 
 app = Flask(__name__)
 app.static_folder = 'static'
