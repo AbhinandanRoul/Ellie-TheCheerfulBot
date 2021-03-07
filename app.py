a71d54bf4c754c83391dfa0fe63308ffa9627c4f
Abhinandan Roul <53302086+AbhinandanRoul@users.noreply.github.com>
2021-03-07 13:05:13 +0530

    init

---
+++ app.py
@@ -0,0 +1,22 @@
+from chatterbot import ChatBot
+from flask import Flask, render_template, request
+from chatterbot.trainers import ChatterBotCorpusTrainer
+
+chatbot = ChatBot('Ron Obvious')
+trainer = ChatterBotCorpusTrainer(chatbot)
+trainer.train("chatterbot.corpus.english")
+
+app = Flask(__name__)
+app.static_folder = 'static'
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+@app.route("/get")
+def get_bot_response():
+    userText = request.args.get('msg')
+    return str(chatbot.get_response(userText))
+
+if __name__ == "__main__":
+    app.run()
